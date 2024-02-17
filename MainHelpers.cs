54a8f5d4e9139d6c5d9b759b8a14f2276ed366e1
vapil <susulya00@mail.ru>
2024-02-17 10:39:26 +0300

    Добавьте файлы проекта.

---
+++ MainHelpers.cs
@@ -0,0 +1,16 @@
+﻿internal static class MainHelpers
+{
+
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("Таблица умножения на " + i);
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine(i + "*" + j + "=" + j * i);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Main
+{
+    public Main()
+    {
+    }
+public static void Main1(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("Таблица умножения на " + i);
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine(i + "*" + j + "=" + j * i);
+            }
+        }
+    }
+}
